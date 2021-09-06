52dceb03fff1c72decc720992b69a074fcb3935a
Alpha <mavamalonga.alpha@gmail.com>
2021-09-07 00:28:18 +0200

    add fonction order by

--- model.py
+++ model.py
@@ -3,14 +3,11 @@ from tinydb import TinyDB, Query
 class Database:
 	def __init__(self):
 		self.db = TinyDB('db.json')
-		self.query = Query() 
+		self.Query = Query() 
 		self.tournament_table = self.db.table('Tournament')
 		self.player_table = self.db.table('Player')
 		self.match_table = self.db.table('Match')
 		self.rounds_table = self.db.table('Rounds')
-
-	def exemple(self):
-		Database.player_table.search(self.query.name == 't')
 
 	def add_tournement(self, name, place, date, number_of_turns, 
 		players, time_control, description):
@@ -32,13 +29,21 @@ class Database:
 			"firstname": firstname,
 			"birthday":birthday,
 			"gender": gender,
-			"ranking": "NA"
+			"ranking": 0
 		}
 		self.player_table.insert(data)
 
-	def select_players(self):
+	def select_players(self, order_by_name=True):
 		players = self.player_table.all()
-		return players
+		if order_by_name:
+			players_order = sorted(players, key=lambda k: k['name'])
+		else:
+			players_order = sorted(players, key=lambda k: k['ranking'])
+		return players_order
+
+	def select_player_where_id(self, player_id):
+		player = self.player_table.get(doc_id = player_id)
+		return player
 
 	def add_match(self, player1, player2):
 		Match = {
@@ -53,3 +58,6 @@ class Database:
 			"matchs": matchs
 		}
 		self.rounds_table.insert(Round)
+
+	def drop_database(self):
+		pass
